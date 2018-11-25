821980bbc11ea560e4dbb36f2f450458e262dd96
prashant8689 <itsmeprashantsingh@yahoo.com>
2018-11-26 00:42:28 +0530

    Add files via upload
    scipy coding done.

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 # main file
 from numpy_data_reader import *
 from matplotlib_plot_data import *
+from scipy_usage import *
 
 
 def main():
     header, data = read_csv()
     plot_data(header, data)
     plot_histogram(data)
+    file_io()
+    linear_algebra_operation()
+    scipy_interpolation()
+    scipy_optimization_fit()
+    scipy_statistics()
+    scipy_image_manipulation()
 
 
 if __name__ == "__main__":
